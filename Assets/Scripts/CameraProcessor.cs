5522ae6c04805eb34237f177a92544d2a41b5f3c
Shanks <maher.manoubi@gmail.com>
2018-10-07 14:59:17 -0700

    Setting up Scenario and Scripts

---
+++ Assets/Scripts/CameraProcessor.cs
@@ -0,0 +1,13 @@
+﻿/// Copyright (c) 2018 All Rights Reserved. Maher Manoubi.
+using UnityEngine;
+
+public class CameraProcessor : MonoBehaviour
+{
+    public Transform UnitToFollow;
+
+	void Update ()
+    {
+        Vector3 newPosition = new Vector3(UnitToFollow.position.x, transform.position.y, UnitToFollow.position.z);
+        transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/MoverProcessor.cs
@@ -0,0 +1,13 @@
+﻿/// Copyright (c) 2018 All Rights Reserved. Maher Manoubi.
+using UnityEngine;
+
+public class MoverProcessor : MonoBehaviour
+{
+    public int Speed = 1;
+    public Vector3 Direction;
+
+	void Update ()
+    {
+        transform.position += ((Direction * Speed) * Time.deltaTime);
+	}
+}
