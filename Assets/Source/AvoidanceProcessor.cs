ff58bd69e29c07a4a473975c0cb3501c3d24319d
Shanks <maher.manoubi@gmail.com>
2018-10-11 20:58:41 -0700

    Code Re-use

--- Assets/Source/AvoidanceProcessor.cs
+++ Assets/Source/AvoidanceProcessor.cs
@@ -19,58 +19,39 @@ public class AvoidanceProcessor : MonoBehaviour
     {
         foreach (UnitComponent neighbor in mUnitComponent.Neighbors)
         {
-            float distanceSq = (neighbor.Position - mUnitComponent.Position).sqrMagnitude;
-            float radiusSq = Mathf.Pow((neighbor.Radius + mUnitComponent.Radius), 2);
-            if (distanceSq != radiusSq)
-            {
-                if (distanceSq < radiusSq)
-                {
-                    radiusSq = Mathf.Pow((neighbor.Radius + mUnitComponent.Radius - Mathf.Sqrt(distanceSq)), 2);
-                }
-
-                Vector2 w = neighbor.Position - mUnitComponent.Position;
-                Vector2 v = mUnitComponent.Velocity - neighbor.Velocity;
-                float a = Vector2.Dot(v, v);
-                float b = Vector2.Dot(w, v);
-                float c = Vector2.Dot(w, w) - radiusSq;
-                float discr = ((b * b) - (a * c));
-                if (discr > .0f && (a < -kEpsilon || a > kEpsilon))
-                {
-                    discr = Mathf.Sqrt(discr);
-                    float t = ((b - discr) / a);
-                    if (t > 0)
-                    {
-                        mUnitComponent.Acceleration += -kScaling * Mathf.Exp(-t / kExponentialCutoff) * (v - (b * v - a * w) / discr) / (a * Mathf.Pow(t, kPowerLawExponent)) * (kPowerLawExponent / t + 1 / kExponentialCutoff);
-                    }
-                }
-            }
+            PowerLawMath(mUnitComponent, neighbor.Position, neighbor.Radius);
         }
 
         foreach (ObstacleComponent obstacle in mUnitComponent.Obstacles)
         {
-            float distanceSq = (obstacle.Position - mUnitComponent.Position).sqrMagnitude;
-            float radiusSq = Mathf.Pow((obstacle.Radius + mUnitComponent.Radius), 2);
-            if (distanceSq != radiusSq)
-            {
-                if (distanceSq < radiusSq)
-                {
-                    radiusSq = Mathf.Pow((obstacle.Radius + mUnitComponent.Radius - Mathf.Sqrt(distanceSq)), 2);
-                }
+            PowerLawMath(mUnitComponent, obstacle.Position, obstacle.Radius);
+        }
+    }
 
-                Vector2 w = obstacle.Position - mUnitComponent.Position;
-                Vector2 v = mUnitComponent.Velocity;
-                float a = Vector2.Dot(v, v);
-                float b = Vector2.Dot(w, v);
-                float c = Vector2.Dot(w, w) - radiusSq;
-                float discr = ((b * b) - (a * c));
-                if (discr > .0f && (a < -kEpsilon || a > kEpsilon))
+    private void PowerLawMath(UnitComponent mUnitComponent, Vector2 obstaclePosition, float obstacleRadius)
+    {
+        float distanceSq = (obstaclePosition - mUnitComponent.Position).sqrMagnitude;
+        float radiusSq = Mathf.Pow((obstacleRadius + mUnitComponent.Radius), 2);
+        if (distanceSq != radiusSq)
+        {
+            if (distanceSq < radiusSq)
+            {
+                radiusSq = Mathf.Pow((obstacleRadius + mUnitComponent.Radius - Mathf.Sqrt(distanceSq)), 2);
+            }
+
+            Vector2 w = obstaclePosition - mUnitComponent.Position;
+            Vector2 v = mUnitComponent.Velocity;
+            float a = Vector2.Dot(v, v);
+            float b = Vector2.Dot(w, v);
+            float c = Vector2.Dot(w, w) - radiusSq;
+            float discr = ((b * b) - (a * c));
+            if (discr > .0f && (a < -kEpsilon || a > kEpsilon))
+            {
+                discr = Mathf.Sqrt(discr);
+                float t = ((b - discr) / a);
+                if (t > 0)
                 {
-                    discr = Mathf.Sqrt(discr);
-                    float t = ((b - discr) / a);
-                    if (t > 0)
-                    {
-                        mUnitComponent.Acceleration += -kScaling * Mathf.Exp(-t / kExponentialCutoff) * (v - (b * v - a * w) / discr) / (a * Mathf.Pow(t, kPowerLawExponent)) * (kPowerLawExponent / t + 1 / kExponentialCutoff);
-                    }
+                    mUnitComponent.Acceleration += -kScaling * Mathf.Exp(-t / kExponentialCutoff) * (v - (b * v - a * w) / discr) / (a * Mathf.Pow(t, kPowerLawExponent)) * (kPowerLawExponent / t + 1 / kExponentialCutoff);
                 }
             }
         }
